d145298b72830cf0fb4d9d13931d0b5bec9096b2
eyewande2022 <116227096+eyewande2022@users.noreply.github.com>
2023-08-31 18:26:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('About to work on Git Actions CI!')
+  res.send('My server CI works excellently!')
 })
 
 app.listen(port, () => {
