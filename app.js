7aebcbb856f955353c1f543d9884da0a802f3226
eyewande2022 <116227096+eyewande2022@users.noreply.github.com>
2023-09-29 07:05:58 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('New CI server successfully and ready for prod!')
+  res.send('New CI server successfully and ready for production!')
 })
 
 app.listen(port, () => {
